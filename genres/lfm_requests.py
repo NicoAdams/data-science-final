4cea8b45b2488115b0e315f62f5b814883583e04
Nico <dominic_adams@brown.edu>
2016-04-19 23:49:07 -0400

    Wrote code that converts song title/artist to genre. Added data files

--- genres/lfm_requests.py
+++ genres/lfm_requests.py
@@ -1,4 +1,4 @@
-# Imports the parent directory
+# Allows importing from the parent directory
 import inspect, os, sys
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
@@ -6,11 +6,17 @@ sys.path.append(parentdir)
 
 import requests
 import xml_parse
+import json
 
+# Configuration data for Last.FM requests
 apiKey = "5da947c52b1de7e8daff6141a97a3af7"
 baseUrl = "http://ws.audioscrobbler.com/2.0/"
 timeout = 12
 maxRequests = 1
+
+# Genre classification setup
+genreFile = "./genre_list_clean"
+tagSignificance = 10 # Minimum tag significance
 
 # -- Requests --
 
@@ -66,20 +72,61 @@ def parseTag(tagTree):
 
 def significantTag(tag):
 	# Returns True if the tag is significant by count, else false
-	return tag['count'] >= 5
+	return tag['count'] >= tagSignificance
+
+def getGenres(tags):
+	# Returns the tags from a tag list that are valid genres
+	
+	# Loads the genre list
+	genresMaster = open(genreFile).readlines()
+	# map(lambda s: s.strip(), genresMaster)
+	for i in range(len(genresMaster)):
+		genresMaster[i] = genresMaster[i].strip()
+	
+	genres = []
+	for tag in tags:
+		tag['name'] = tag['name'].lower()
+		for genre in genresMaster:
+			if tag['name'] == genre:
+				genres.append(tag)
+				break
+	return genres
 
 def getSignificantTags(topTagsTree):
 	# Returns the most significant tag names for the given track
 	tagTrees = topTagsTree.getchildren()[0].getchildren()
 	tags = map(parseTag, tagTrees)
 	
-	# Processing step: Correlating names & combining counts
-	
+	# Filters tags with low significance
 	tags = filter(significantTag, tags)
+	
 	return tags
 
 def requestSignificantTags(trackName, artistName):
+	# Returns a 
 	r = requestTrackTopTags(trackName, artistName)
 	rTree = xml_parse.parse(r.text.encode('utf-8'))
 	return getSignificantTags(rTree)
 
+# -- Data compiling --
+
+def requestGenresFromSong(songInfo):
+	""" Requests a song's tags from its info dictionary
+	songInfo: A dictionary with fields "title" and "artist"
+	"""
+	tags = requestSignificantTags(songInfo['title'], songInfo['artist'])
+	return getGenres(tags)
+
+def generateGenreData(songsFile, genresFile):
+	""" Obtains the genres dataset from the songs dataset
+	"""
+	songsFileData = json.load(open(songsFile))
+	
+	currGenreData = json.load(open(genresFile))
+	
+	genreData = {}
+	for songID in songsFileData:
+		songInfo = songsFileData[songID]
+		genres = requestGenresFromSong(songInfo)
+		genreData[songID] = genres
+	
