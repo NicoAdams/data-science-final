ddcebdee27f651271e415aafbf532cd973ac629f
Alex Ashery <alexander_ashery@brown.edu>
2016-05-13 15:30:29 -0400

    Commented some code

--- chartlyrics/chartlyrics_interface.py
+++ chartlyrics/chartlyrics_interface.py
@@ -2,12 +2,21 @@ import requests
 import xml.etree.ElementTree as ET
 
 class ChartLyricsInterface(object):
-
+    '''
+    Python interface for interacting with the ChartLyrics API
+    '''
     prefix_url = 'http://api.chartlyrics.com/apiv1.asmx/'
 
     def __init__(self): pass
 
     def __api_call(self, command, argnames, args):
+        '''
+        Submits a call to the ChartLyrics API
+        :param command: API call the user wishes to perform
+        :param argnames: a list of argument names for the API call
+        :param args: a list of arguments corresponding to each argument name
+        :return: the response from the API if the call is successful, otherwise None
+        '''
         assert len(argnames) == len(args), 'Number of argnames must match number of args'
         url = self.prefix_url + command + '?' + self.__format_query(argnames, args)
         try:
@@ -17,14 +26,32 @@ class ChartLyricsInterface(object):
             return None
 
     def __format_query(self, argnames, args):
+        '''
+        Formats a query
+        :param argnames: a list of argument names for an API call
+        :param args: a list of arguments corresponding to each argument name
+        :return: a string formatted for the ChartLyrics API using the given argument names and values
+        '''
         args = [arg.lower().strip().replace(' ', '%20') for arg in args]
         return '&'.join([argname + '=' + arg for argname, arg in zip(argnames, args)])
 
     def search_lyric(self, artist, song):
+        '''
+        Returns response from calling SearchLyric on the API
+        :param artist: the artist to look for
+        :param song: the song by the given artist to look for
+        :return: xml returned from the API call
+        '''
         xml = self.__api_call('SearchLyric', ['artist', 'song'], [artist, song])
         return xml
 
     def search_lyric_direct(self, artist, song):
+        '''
+        Returns response from calling SearchLyricDirect on the API
+        :param artist: the artist to look for
+        :param song: the song by the given artist to look for
+        :return: the lyrics for the given song if the call is successful, otherwise None
+        '''
         xml = self.__api_call('SearchLyricDirect', ['artist', 'song'], [artist, song])
         if xml is None:
             return None
@@ -32,13 +59,24 @@ class ChartLyricsInterface(object):
         return root[-1].text
 
     def search_lyric_text(self, lyricText):
+        '''
+        Returns response from calling SearchLyricText on the API
+        :param lyricText: lyric to look for
+        :return: xml returned from the API call
+        '''
         xml = self.__api_call('SearchLyricText', ['lyricText'], [lyricText])
         return xml
 
     def get_lyric(self, lyricId, lyricCheckSum):
+        '''
+        Returns response from calling GetLyric on the API
+        :param lyricId: unique lyric id to look for
+        :param lyricCheckSum: lolidk
+        :return: xml returned from the API call
+        '''
         xml = self.__api_call('GetLyric', ['lyricId', 'lyricCheckSum'], [lyricId, lyricCheckSum])
         return xml
 
 if __name__ == "__main__":
     interface = ChartLyricsInterface()
-    print interface.search_lyric_direct("Michael Jackson", "Bad")
+    print interface.search_lyric("Michael Jackson", "Bad")
--- feature_extractor.py
+++ feature_extractor.py
@@ -4,6 +4,9 @@ import re
 import random
 
 class FeatureExtractor(object):
+    '''
+    FeatureExtractor adjusts an input string so that it has a certain set of features
+    '''
     def __init__(self, tokenizer, stopwordsfilename=None, sentimentfilename=None, randomizeSentiments=False):
         self.set_tokenizer(tokenizer)
         self.set_stopwords(stopwordsfilename)
@@ -11,9 +14,17 @@ class FeatureExtractor(object):
         self.__r = re.compile(r'\W+')
 
     def set_tokenizer(self, tokenizer):
+        '''
+        :param tokenizer: a tokenizer
+        :return: nothing, sets the class' internal tokenizer
+        '''
         self.__tokenizer = tokenizer
 
     def set_stopwords(self, stopwordsfilename):
+        '''
+        :param stopwordsfilename: path to file of stop words list
+        :return: nothing, sets the stop word list
+        '''
         if stopwordsfilename is not None:
             with open(stopwordsfilename, 'r') as f:
                 self.__stopwords = [word.lower().strip() for word in f]
@@ -21,12 +32,20 @@ class FeatureExtractor(object):
             self.__stopwords = None
     
     def __shuffle_sentiment(self):
+        '''
+        :return: nothing, shuffles sentiment values in (word, sentiment) dictionary
+        '''
         words = self.__sentiments.keys()
         randWords = list(words)
         random.shuffle(randWords)
         self.__sentiments = {randWords[i]: self.__sentiments[words[i]] for i in xrange(len(words))}    
     
     def set_sentiment(self, sentimentfilename, randomizeSentiments=False):
+        '''
+        :param sentimentfilename: path to file of sentiment database
+        :param randomizeSentiments: option to randomly reassign sentiments to words, for testing classifier
+        :return: nothing, sets the (word, sentiment) dictionary
+        '''
         if sentimentfilename is not None:
             with open(sentimentfilename, 'r') as f:
                 reader = csv.reader(f, delimiter='\t')
@@ -39,16 +58,32 @@ class FeatureExtractor(object):
             self.__sentiments = None
 
     def __filter_stopwords(self, words):
+        '''
+        :param words: a list of words
+        :return: list of words with all letters lowercased, and stop words filtered out
+        '''
         return [word.lower() for word in words if word.lower() not in self.__stopwords] if self.__stopwords else words
 
     def __split(self, inputStr):
+        '''
+        :param inputStr: a string to be split into words
+        :return: string split into words (according to regexp), with stop words filtered out
+        '''
         words = self.__filter_stopwords(self.__r.split(inputStr))
         return [word for word in words if word != '']
 
     def __get_sentiment(self, word):
+        '''
+        :param word: a word to find the sentiment value of
+        :return: sentiment value of the word
+        '''
         return self.__sentiments[word] if word in self.__sentiments else 0.
 
     def __count_words(self, words):
+        '''
+        :param words: a list of strings
+        :return: a list of each word, paired with the number of times it appeared in the input list
+        '''
         word_count_pairs = {}
         for word in words:
             if word in word_count_pairs:
@@ -58,6 +93,13 @@ class FeatureExtractor(object):
         return word_count_pairs.items()
 
     def __sort_by_sent(self, inputStr, binary, reverse=False):
+        '''
+        :param inputStr: a string to be parsed
+        :param binary: option to make binary feature vector (True) or count feature vector (False)
+        :param reverse: option to have list sorted in reverse
+        :return: the words of the input string, sorted by sentiment in the direction of reverse, and with number of
+                 words preserved if binary is False
+        '''
         words = self.__split(inputStr)
         if binary:
             word_sent_pairs = [(word, self.__get_sentiment(word)) for word in set(words)]
@@ -68,28 +110,36 @@ class FeatureExtractor(object):
         return self.__remove_sent(sorted_words)
 
     def __remove_sent(self, word_sent_pairs):
+        '''
+        :param word_sent_pairs: a list of (word, sentiment) pairs
+        :return: a list of each word
+        '''
         return [word for word, sent in word_sent_pairs]
 
     def __repeat_words(self, word_count_pairs):
+        '''
+        :param word_count_pairs: a list of (word, count) pairs
+        :return: a list consisiting of each word repeated count times
+        '''
         unflattened_list = [[word for i in range(count)] for word, count in word_count_pairs]
         return [word for sublist in unflattened_list for word in sublist]
 
     def extract_all(self, inputStr, binary=True):
         '''
-        Makes a feature vector using all words from the given string.
+        Makes a new string for a feature vector using all words from the given string.
         :param inputStr: an input string
         :param binary: option to make binary feature vector (True) or count feature vector (False)
-        :return: Feature vector made from all words in the input
+        :return: String for feature vector made from all words in the input
         '''
         return inputStr
 
     def extract_top_n(self, inputStr, n, binary=True):
         '''
-        Makes a feature vector using the top n sentiment words from the given string.
+        Makes a new string for a feature vector using the top n sentiment words from the given string.
         :param input: an input string
         :param n: the number of top words to keep
         :param binary: option to make a binary feature vector (True) or count feature vector (False)
-        :return: Feature vector made from the top n words in the input
+        :return: String for feature vector made from the top n words in the input
         '''
         assert self.__sentiments is not None
         assert n >= 1
@@ -103,11 +153,11 @@ class FeatureExtractor(object):
 
     def extract_bottom_n(self, inputStr, n, binary=True):
         '''
-        Makes a feature vector using the bottom n sentiment words from the given string
+        Makes a new string for a feature vector using the bottom n sentiment words from the given string
         :param inputStr: an input string
         :param n: the number of top words to keep
         :param binary: option to make a binary feature vector (True) or count feature vector (False)
-        :return: feature vector made from the bottom n words in the input
+        :return: string for feature vector made from the bottom n words in the input
         '''
         assert self.__sentiments is not None
         assert n >= 1
@@ -121,12 +171,12 @@ class FeatureExtractor(object):
 
     def extract_top_n_bottom_m(self, inputStr, n, m, binary=True):
         '''
-        Makes a feature vector using the top n and bottom m words from the given string
+        Makes a new string for a feature vector using the top n and bottom m words from the given string
         :param inputStr: an input string
         :param n: the number of top words to keep
         :param m: the number of bottom words to keep
         :param binary: option to make a binary feature vector (True) or count feature vector (False)
-        :return: feature vector made from the top n and bottom m words in the input
+        :return: string for feature vector made from the top n and bottom m words in the input
         '''
 
         assert self.__sentiments is not None
